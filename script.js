f4f81d684a186387a3439cb861d1b943fd09d20b
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-03-13 00:47:44 -0800

    Renaming pages

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 start.addEventListener("click",function() {
     console.log ("button was clicked")
 });
+
+yourScore.addEventListener("click",function(){
+    // prevent.default();
+    
+    scorePage.setAttribute();
+})
